be9b40321db6d2a0c8571ccff63c4925eaa19ddc
hawwestin <michal.robaszewski+git@gmail.com>
2026-05-01 19:32:26 +0200

    ansible snippets

--- Ansible/snippets/get-inventory.sh
+++ Ansible/snippets/get-inventory.sh
@@ -1,1 +1,7 @@
+export ANSIBLE_CONFIG=$PWD/ansible/ansible.cfg
+
 ansible -i Ansible/inventory --list-hosts all
+ansible -i Ansible/inventory --list-hosts homelab
+
+# Or, to get the full inventory in YAML format:
+ansible-inventory --list -y
